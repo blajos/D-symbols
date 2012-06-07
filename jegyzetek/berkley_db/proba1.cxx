626d4ca4bcbcfbe6d97634a58c736ec6785c7c7e
Boroczki, Lajos <boroczki.lajos@gmail.com>
2012-06-07 09:07:28 +0200

    BME workshopra eloadas

--- jegyzetek/berkley_db/proba1.cxx
+++ jegyzetek/berkley_db/proba1.cxx
@@ -2,10 +2,10 @@
 
 int main(int argc, char *argv[]){
   Db db(0,0);
-  db.open(NULL, "./dbfile.db", NULL, DB_BTREE, DB_CREATE, 0664);
+  db.open(NULL, "./dbfile.db", NULL, DB_HASH, DB_CREATE, 0664);
 
   std::string *teszt = new std::string("Grocery bill.");
-  int a=0;
+  bool a=true;
 
   Dbt key((void*)teszt->c_str(), teszt->size() + 1);
   Dbt data(&a, sizeof(a));
